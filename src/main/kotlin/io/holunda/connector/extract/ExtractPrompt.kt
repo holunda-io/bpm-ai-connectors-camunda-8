ad0078415be7ff7fa13702a31a6d7a34c48a13ca
Benjoyo <bennet.krause@gmx.de>
2023-05-02 12:14:37 +0200

    small fixes

--- src/main/kotlin/io/holunda/connector/extract/ExtractPrompt.kt
+++ src/main/kotlin/io/holunda/connector/extract/ExtractPrompt.kt
@@ -23,7 +23,7 @@ class ExtractPrompt(
 
     companion object {
         private val SYSTEM_PROMPT = """
-            You are an extremely clever data extraction AI. You love to extract data and information from an input text and output correct results. 
+            You are an extremely clever information extraction AI. You love to extract data and information from an input text and output correct results. 
             
             You will receive a JSON with input values to extract information from.
             If the input JSON does not contain some of the required information, set the corresponding value in the result to null.
