7d7dc80d4043c7a8631f55bcd61107390f270516
Benjoyo <bennet.krause@gmx.de>
2023-08-23 16:11:00 +0200

    fixes and improvements

--- core/src/main/kotlin/io/holunda/connector/compose/ComposeRequest.kt
+++ core/src/main/kotlin/io/holunda/connector/compose/ComposeRequest.kt
@@ -5,7 +5,7 @@ import io.holunda.connector.common.*
 
 data class ComposeRequest(
     val inputJson: JsonNode,
-    val description: String,
+    var description: String,
     val style: String,
     val type: String,
     val tone: String,
