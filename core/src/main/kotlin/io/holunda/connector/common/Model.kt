6c72bda00ff4da511126ea450d4d345c34493fe7
Benjoyo <bennet.krause@gmx.de>
2023-10-12 13:18:26 +0200

    update element templates and remove active job limit

--- core/src/main/kotlin/io/holunda/connector/common/Model.kt
+++ core/src/main/kotlin/io/holunda/connector/common/Model.kt
@@ -4,5 +4,5 @@ enum class Model(val modelId: String) {
     GPT_3("gpt-3.5-turbo"),
     GPT_4("gpt-4"),
     LUMINOUS_SUPREME("luminous-supreme"),
-    COHERE_COMMAND_XLARGE("cohere-command-xlarge")
+    COHERE_CHAT_COMMAND("cohere-chat-command")
 }
